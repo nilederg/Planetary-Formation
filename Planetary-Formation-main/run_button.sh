911390c7aebbd7548998ce1ec59765eaa4b81729
Nile <nilederg@gmail.com>
2023-03-28 11:15:06 -0700

    Delete deprecated infrastructure

--- Planetary-Formation-main/run_button.sh
+++
@@ -1,3 +0,0 @@
-# Next line will run the static void main it finds within the class you give
-java planetaryFormation
-# Note: currently, the Replit debugger works only if the class you're running is called Main

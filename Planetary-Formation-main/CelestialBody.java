b51960312c8bd0b74f9dc80e4b0c02915e2760a4
Nile <nilederg@gmail.com>
2023-05-24 09:45:29 -0700

    Revert "Implementing Planet class"
    This reverts commit 49aec6d36dfd2bd28655da1211a46433c65dc22b.

--- Planetary-Formation-main/CelestialBody.java
+++
@@ -1,5 +0,0 @@
-public class Planet extends Particle{
-    // Composition
-
-
-}
--- Planetary-Formation-main/particle.java
+++ Planetary-Formation-main/particle.java
@@ -1,10 +1,8 @@
-package Positionals;
-
-public class Particle{
-  Vector position; // Meters from origin
-  Vector velocity; // Meters/sec
-  double radius; // Meters
-  double mass; // Kg
+class Particle{ 
+  Vector position;
+  Vector velocity;
+  double radius;
+  double mass;
   // 1 distance away and 1 mass, should produce 1 force
   void applyForce(double force, double interval) {
     // 1 force at 1 interval produces 1 distance change

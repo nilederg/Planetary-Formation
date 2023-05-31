b00c5d8440ea80b5ee1e01e6bf2fbeb5fac09c14
nilederg <nilederg@gmail.com>
2023-05-31 01:03:47 -0700

    Testing code

--- Planetary-Formation-main/planetaryFormation.java
+++ Planetary-Formation-main/planetaryFormation.java
@@ -1,12 +1,15 @@
-import java.util.Random;
+import Storage.Positionals.Vector3;
+
 import java.util.Scanner;
 
 public class planetaryFormation {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    /*Scanner sc = new Scanner(System.in);
     Random random = new Random();
-    double radius = getDouble("Planetary radius (meters): ", sc);
-
+    double radius = getDouble("Planetary radius (meters): ", sc);*/
+    Vector3 test = new Vector3(new double[]{1, 0.1, 1.3});
+    test.normalizeCube();
+    test.println();
   }
 
   static double getDouble(String prompt, Scanner sc) {

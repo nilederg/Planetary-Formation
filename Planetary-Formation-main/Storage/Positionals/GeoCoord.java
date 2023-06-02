33e7d35d32f96383e79bf3ea4a8e7bb2f1567dce
Isaac Eilebrecht <57379634+ILHS1118422@users.noreply.github.com>
2023-06-02 14:59:16 -0700

    boilerplate

--- Planetary-Formation-main/Storage/Positionals/GeoCoord.java
+++ Planetary-Formation-main/Storage/Positionals/GeoCoord.java
@@ -1,9 +1,21 @@
 package Storage.Positionals;
 
-// Against my better judgement, a class without boilerplate
+import java.util.Objects;
+
+// Against my better judgement, a class without boilerplate (not anymore ATTICUS)
 // I will NEVER need more than this in geoCoord, it's simply cleaner than sending a 2 double array.
 // I am confident in this one violation of encapsulation principles.
-public class GeoCoord {
-    public double latitude;
-    public double longitude;
+public record GeoCoord(double latitude, double longitude) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GeoCoord geoCoord = (GeoCoord) o;
+        return Double.compare(geoCoord.latitude, latitude) == 0 && Double.compare(geoCoord.longitude, longitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(latitude, longitude);
+    }
 }

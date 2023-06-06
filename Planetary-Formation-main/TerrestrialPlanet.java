874120bede9afbbc1c8d534521fad5b3c19b0833
Isaac Eilebrecht <nilederg@gmail.com>
2023-06-06 15:01:20 -0700

    Start noise planet

--- Planetary-Formation-main/TerrestrialPlanet.java
+++ Planetary-Formation-main/TerrestrialPlanet.java
@@ -1,3 +1,16 @@
-public class TerrestrialPlanet {
+import Storage.Positionals.Noise3;
+import Storage.ScalarSphere;
 
+public class TerrestrialPlanet {
+    private ScalarSphere terrain;
+
+    public void fractalNoise(int depth) {
+        Noise3[] layers = new Noise3[depth];
+
+        for (int i = 0; i < depth; i ++) {
+            int freq = (int) Math.pow(2, i);
+            layers[i] = new Noise3(new int[] {freq, freq, freq});
+
+        }
+    }
 }

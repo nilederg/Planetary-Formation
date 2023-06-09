cf71c53606ab58f1fa654256af2cb796f5d70ed1
nilederg <nilederg@gmail.com>
2023-06-09 04:34:53 -0700

    new unit testing

--- Planetary-Formation-main/planetaryFormation.java
+++ Planetary-Formation-main/planetaryFormation.java
@@ -1,14 +1,19 @@
 import Storage.Positionals.GeoCoord;
 import Storage.Positionals.Vector3;
-import Storage.ScalarSphere;
+import Storage.STL.TriangleFace;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class planetaryFormation {
   public static void main(String[] args) {
-    ScalarSphere sphere = new ScalarSphere(5);
-    sphere.
-    System.out.println(sphere.getPoint(new GeoCoord(40, 20)));
+    TerrestrialPlanet planet = new TerrestrialPlanet(2, 10000000000L);
+    planet.initFractalNoise(5, 6);
+    System.out.println(planet.terrain.getPoint(new GeoCoord(Math.toRadians(40), Math.toRadians(20))));
+    System.out.println(planet.terrain.getPoint(new GeoCoord(Math.toRadians(40), Math.toRadians(21))));
+    System.out.println(planet.terrain.getPoint(new GeoCoord(Math.toRadians(90), Math.toRadians(128))));
+
+    System.out.println(Arrays.toString(TriangleFace.fromOrientingPoint(new Vector3(new double[]{0, 0, 0}), true, new Vector3[]{new Vector3(new double[]{9, 4, 5}), new Vector3(new double[]{9, 3, 8}), new Vector3(new double[]{16, 49, 1})}).exportCode()));
   }
 
   static double getDouble(String prompt, Scanner sc) {

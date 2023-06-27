0ee98b1a226c456145bac0617d04dbe983c61838
Isaac <1118422@apps.nsd.org>
2023-06-27 06:52:46 -0700

    Buffered file writer

--- Planetary-Formation-main/storage/STL/StlFile.kt
+++ Planetary-Formation-main/storage/STL/StlFile.kt
@@ -6,6 +6,9 @@ import java.io.IOException
 class StlFile constructor(filename: String, unit: String, header: String) {
     private val file: FileWriter
     private var triCount: Int
+    private val buffer: CharArray
+    private var bufferUsage: Int
+
     fun getTriCount(): Int {
         return triCount
     }
@@ -15,20 +18,39 @@ class StlFile constructor(filename: String, unit: String, header: String) {
     init {
         file = FileWriter(filename)
         triCount = 0
-        if (header.length != 70) throw IllegalArgumentException("Header must be 70 bytes")
+        buffer = CharArray(65536)
+        bufferUsage = 0
+        if (header.length > 70) throw IllegalArgumentException("Header must be 70 bytes or less")
         if (unit.length != 2) throw IllegalArgumentException("Unit must be 2 bytes (mm,cm, m,ft,in,ly)")
-        file.write("$header\nUNITS=$unit\n    ")
+        if (header.length < 70) {
+            file.write("$header\nUNITS=$unit" +
+                    String(CharArray(70-header.length) { return@CharArray ' ' }) +
+                    "\n    ")
+        } else {
+            file.write("$header\nUNITS=$unit\n    ")//TODO Write to triangle count
+        }
     }
 
     @Throws(IOException::class)
     fun writeTriangle(triangle: TriangleFace) {
         triCount++
         val code: ByteArray = triangle.exportCode()
-        for (codeByte: Byte in code) file.append(Char(codeByte.toUShort()))
+        for (codeByte: Byte in code) buffer[bufferUsage] = Char(codeByte.toUShort())
+        bufferUsage++
+        if (bufferUsage == 65536) {
+            file.append(buffer.toString())
+            bufferUsage = 0
+        }
     }
 
     @Throws(IOException::class)
     fun writeTriangles(triangles: Array<TriangleFace>) {
         for (triangle: TriangleFace in triangles) writeTriangle(triangle)
+    }
+
+    fun close() {
+        val remainingBuffer: CharArray = CharArray(bufferUsage) { i: Int -> return@CharArray buffer[i] }
+        file.append(remainingBuffer.toString())
+        file.close()
     }
 }
